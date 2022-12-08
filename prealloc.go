d91283a29452831e7fd0844cb5b2f721b2c298fd
LU yao <luyao@yunshan.net.cn>
2022-12-08 17:38:20 +0800

    revert prealloc.go

--- prealloc.go
+++ prealloc.go
@@ -1,11 +1,11 @@
 package tempopb
 
-//import "github.com/grafana/tempo/pkg/tempopb/pool"
+import "github.com/grafana/tempo/pkg/tempopb/pool"
 
-/* var (
+var (
 	// buckets: [0.5KiB, 1KiB, 2KiB, 4KiB, 8KiB, 16KiB] ...
 	bytePool = pool.New(500, 64_000, 2, func(size int) []byte { return make([]byte, 0, size) })
-) */
+)
 
 // PreallocBytes is a (repeated bytes slices) which preallocs slices on Unmarshal.
 type PreallocBytes struct {
@@ -14,9 +14,9 @@ type PreallocBytes struct {
 
 // Unmarshal implements proto.Message.
 func (r *PreallocBytes) Unmarshal(dAtA []byte) error {
-	/* r.Slice = bytePool.Get(len(dAtA))
+	r.Slice = bytePool.Get(len(dAtA))
 	r.Slice = r.Slice[:len(dAtA)]
-	copy(r.Slice, dAtA) */
+	copy(r.Slice, dAtA)
 	return nil
 }
 
@@ -37,7 +37,7 @@ func (r *PreallocBytes) Size() (n int) {
 
 // ReuseByteSlices puts the byte slice back into bytePool for reuse.
 func ReuseByteSlices(buffs [][]byte) {
-	/* for _, b := range buffs {
+	for _, b := range buffs {
 		bytePool.Put(b[:0])
-	} */
+	}
 }
